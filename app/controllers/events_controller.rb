54ed90709bb60391bab8c2bc15b3dfdc10b40c1c
Robert Neal <robertwneal2@gmail.com>
2022-11-01 16:05:09 -0700

    Create basic events#index and events#show views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,12 @@
 class EventsController < ApplicationController
 
   def index
+    @events = Event.all
+  end
 
+  def show
+    @event = Event.find(params[:id])
+    @params = params
   end
 
 end
