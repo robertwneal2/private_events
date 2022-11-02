4a359597f38229fc89e0a931f2af902473dcc9c4
Robert Neal <robertwneal2@gmail.com>
2022-11-01 17:20:56 -0700

    Create User routes and views for users#index and users#show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,7 +6,6 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
-    @params = params
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :events
+  resources :events, :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
