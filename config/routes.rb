90c43f443c011c3938dbc9d350833a54429f9ce4
Robert Neal <robertwneal2@gmail.com>
2022-11-02 16:24:54 -0700

    Add link in header to all Users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :events
   resources :event_attendees
-  resources :users, only: [:show]
+  resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
