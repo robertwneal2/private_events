cd38bf3a18c28f0c82c7e78123ec13464a4bc8da
Robert Neal <robertwneal2@gmail.com>
2022-11-02 07:28:56 -0700

    Add create Event functionality

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
     @events = Event.all
@@ -6,6 +7,24 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    
+    if @event.save
+      redirect_to @event
+    else 
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :date, :location, :user_id)
   end
 
 end
