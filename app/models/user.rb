4aabffbfe8515f708007369aee6374ed00137a1c
Robert Neal <robertwneal2@gmail.com>
2022-11-02 13:37:40 -0700

    Add dependent: :destroy to User and Event models to demonstrate an error

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
 
   has_many :created_events, class_name: "Event", foreign_key: :user_id
   
-  has_many :event_attendees, foreign_key: :user_id
+  has_many :event_attendees, foreign_key: :user_id, dependent: :destroy
   has_many :attended_events, through: :event_attendees, source: :event
 end
